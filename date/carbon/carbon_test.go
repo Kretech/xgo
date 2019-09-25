b92148102901a80e9ee09d6eb7fce3271d803dd3
zhanghuiren <zhrlnt@gmail.com>
2019-09-25 23:28:05 +0800

    carbon

--- date/carbon/carbon_test.go
+++ date/carbon/carbon_test.go
@@ -1,6 +1,9 @@
 package carbon
 
 import (
+	"encoding/json"
+	"fmt"
+	"strconv"
 	"testing"
 	"time"
 
@@ -25,8 +28,8 @@ func TestUnixOf(t *testing.T) {
 }
 
 func TestParse(t *testing.T) {
-	Parse("2012-1-1 01:02:03")
-	Parse("+1 day")
+	parse("2012-1-1 01:02:03")
+	parse("+1 day")
 }
 
 func TestCarbon_Format(t *testing.T) {
@@ -49,7 +52,55 @@ func TestCarbon_In(t *testing.T) {
 }
 
 func TestCarbon_Sub(t *testing.T) {
-	t1 := TParse("Y-m-d H:i:s", "2018-01-02 09:00:00")
+	t1 := ParseUTC("Y-m-d H:i:s", "2018-01-02 09:00:00")
 	t2 := t1.Sub(time.Hour)
 	test.AssertEqual(t, t2.Format("Y-m-d H:i:s"), "2018-01-02 08:00:00")
+}
+
+func TestCarbon_MarshalJSO2N(t *testing.T) {
+
+	t0 := ParseUTC("Y-m-d H:i:s", "1970-01-01 00:00:00")
+	js, err := json.Marshal(t0)
+	test.AssertNil(t, err)
+
+	test.AssertEqual(t, string(js), `0`)
+
+	t1 := ParseUTC("Y-m-d H:i:s", "2018-01-02 01:00:00")
+	js, err = json.Marshal(t1)
+	test.AssertNil(t, err)
+
+	test.AssertEqual(t, string(js), `1514854800`)
+}
+
+func TestCarbon_MarshalJSON(t *testing.T) {
+	type args struct {
+		*time.Location
+		str     string
+		unixStr string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{``, args{UTC, `1970-01-01 00:00:00`, `0`}},
+		{``, args{Shanghai, `1970-01-01 08:00:00`, `0`}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := ParseIn(`Y-m-d H:i:s`, tt.args.str, tt.args.Location)
+			unixBytes, err := c.MarshalJSON()
+
+			test.AssertNil(t, err)
+			test.AssertEqual(t, string(unixBytes), tt.args.unixStr)
+
+			unix, _ := strconv.Atoi(tt.args.unixStr)
+			unix += 3600
+			unixStr := fmt.Sprint(unix)
+			var c2 Carbon
+			err = json.Unmarshal([]byte(unixStr), &c2)
+			test.AssertNil(t, err)
+			test.AssertEqual(t, c.Add(time.Hour).String(), c2.String())
+		})
+	}
 }
