ef4df74c6e7213089af39454001c10f6822881c3
zhanghuiren <zhrlnt@gmail.com>
2019-11-24 23:31:49 +0800

    error wrap

--- pipe/pipe.go
+++ pipe/pipe.go
@@ -24,17 +24,17 @@ func NewPipe(cmd *exec.Cmd) *Pipe {
 func (this *Pipe) Start() (err error) {
 	this.stdinPipe, err = this.cmd.StdinPipe()
 	if err != nil {
-		return errors.Wrap(err, ``)
+		return errors.Wrap(err, `pipe start`)
 	}
 
 	this.stdoutPipe, err = this.cmd.StdoutPipe()
 	if err != nil {
-		return errors.Wrap(err, ``)
+		return errors.Wrap(err, `pipe start`)
 	}
 
 	err = this.cmd.Start()
 	if err != nil {
-		return errors.Wrap(err, ``)
+		return errors.Wrap(err, `pipe start`)
 	}
 
 	this.bufRW = bufio.NewReadWriter(bufio.NewReader(this.stdoutPipe), bufio.NewWriter(this.stdinPipe))
@@ -43,24 +43,24 @@ func (this *Pipe) Start() (err error) {
 }
 
 func (this *Pipe) Stop() {
-	this.stdinPipe.Close()
-	this.stdoutPipe.Close()
-	this.cmd.Wait()
 }
 
 func (this *Pipe) WriteAndRead(b []byte) (resp []byte, err error) {
 	_, err = this.bufRW.Write(b)
 	if err != nil {
-		err = errors.Wrap(err, ``)
+		err = errors.Wrap(err, `pipe write`)
 		return
 	}
 
 	err = this.bufRW.Flush()
 	if err != nil {
-		err = errors.Wrap(err, ``)
+		err = errors.Wrap(err, `pipe flush`)
 		return
 	}
 
 	resp, _, err = this.bufRW.ReadLine()
+	if err != nil {
+		err = errors.Wrap(err, `pipe readLine`)
+	}
 	return
 }
