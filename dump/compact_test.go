1ab509b4a32fc99f7a2d43c829c0976208aea9b5
zhanghuiren <zhrlnt@gmail.com>
2020-03-26 23:09:45 +0800

    dump.Compact

--- dump/compact_test.go
+++ dump/compact_test.go
@@ -9,8 +9,23 @@ import (
 
 func TestDepthCompact(t *testing.T) {
 	a := 3
-	keys, kvs := dump.DepthCompact(0, a)
-	t.Log(keys, kvs)
+	b := `B`
+	t.Run(`DepthCompact`, func(t *testing.T) {
+		keys, kvs := dump.DepthCompact(0, a, b)
+		t.Log(keys, kvs)
+		test.AssertEqual(t, kvs[`a`], 3)
+	})
 
-	test.AssertEqual(t, kvs[`a`], 3)
+	t.Run(`Compact`, func(t *testing.T) {
+		keys, kvs := dump.Compact(a, b)
+		t.Log(keys, kvs)
+		test.AssertEqual(t, kvs[`a`], 3)
+	})
+}
+
+func TestCompact(t *testing.T) {
+	a := 3
+	b := `B`
+	keys, kvs := dump.Compact(a, b)
+	dump.Dump(keys, kvs)
 }
