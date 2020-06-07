81e8db9973ffc7d85f5ab1dd1ecd1cb0d687cd64
huiren <zhrlnt@gmail.com>
2020-06-07 17:03:41 +0800

    HttpClient (#9)
    * httpclient
    
    * readme
    
    * wrapper test
    
    * readme
    
    * go version
    
    * NewRequest in 1.12
    
    * tc timeout

---
+++ httpclient/response.go
@@ -0,0 +1,112 @@
+package httpclient
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+type Response struct {
+	*http.Response
+
+	bytes []byte
+}
+
+func (r *Response) Bytes() (bytes []byte, err error) {
+	if len(r.bytes) > 0 {
+		return r.bytes, nil
+	}
+
+	r.bytes, err = ioutil.ReadAll(r.Response.Body)
+	if err != nil {
+		err = errors.Wrapf(err, "Response.ReadAll")
+		return []byte{}, err
+	}
+
+	return r.bytes, nil
+}
+
+func (r *Response) String() (string, error) {
+	bytes, err := r.Bytes()
+	if err != nil {
+		return ``, err
+	}
+	return string(bytes), nil
+}
+
+var (
+	Unmarshals = map[string]func([]byte, interface{}) error{
+		`application/json`: JsonUnmarshaler,
+		`application/xml`:  XmlUnmarshaler,
+	}
+
+	DefaultUnmarshaler = JsonUnmarshaler
+
+	JsonUnmarshaler = func(b []byte, v interface{}) error { return json.Unmarshal(b, v) }
+	XmlUnmarshaler  = func(b []byte, v interface{}) error { return xml.Unmarshal(b, v) }
+)
+
+func (r *Response) To(v interface{}) error {
+	bytes, err := r.Bytes()
+	if err != nil {
+		return err
+	}
+
+	var found bool
+	contentType := r.Response.Header.Get("Content-Type")
+	for key, un := range Unmarshals {
+		if strings.Contains(contentType, key) {
+			err = un(bytes, v)
+			if err == nil {
+				found = true
+				break
+			}
+
+			err = errors.Wrapf(err, `Unmarshal error with: %s`, bytes)
+			return err
+		}
+	}
+	if !found {
+		if DefaultUnmarshaler == nil {
+			return errors.Errorf(`nonsupport Content-Type: ` + contentType)
+		}
+
+		err = DefaultUnmarshaler(bytes, v)
+		if err != nil {
+			return errors.Wrapf(err, `Unmarshal error with: %s`, bytes)
+		}
+	}
+
+	return nil
+}
+
+func (r *Response) UnwrapTo(w ResponseWrapper, v interface{}) error {
+	w.SetData(v)
+	err := r.To(w)
+	if err != nil {
+		return err
+	}
+
+	err = w.Error()
+	if err != nil {
+		return errors.Wrapf(err, "response.user.error")
+	}
+
+	return nil
+}
+
+func (r *Response) UnwrapToInterface(w ResponseWrapper) (interface{}, error) {
+	var i interface{}
+	err := r.UnwrapTo(w, &i)
+	return i, err
+}
+
+func (r *Response) ToInterface() (interface{}, error) {
+	var i interface{}
+	err := r.To(&i)
+	return i, err
+}
